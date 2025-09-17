1583a92798f7f361bd5af84d93a8837d1b4d54f6
cvUPTZ <cvupdz@gmail.com>
2025-09-17 19:41:15 +0100

    Create index.tsx

---
+++ src/components/documents/index.tsx
@@ -0,0 +1,8 @@
+// Create this file as src/components/documents/index.js
+
+export { default as BusinessModelCanvasDocument } from './BusinessModelCanvasDocument';
+export { default as BusinessPlanDocument } from './BusinessPlanDocument';
+export { default as MarketStudyDocument } from './MarketStudyDocument';
+
+// Then in your App.js or other files, you can import like this:
+// import { BusinessModelCanvasDocument, BusinessPlanDocument, MarketStudyDocument } from './components/documents';
